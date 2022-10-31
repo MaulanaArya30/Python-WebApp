109aa4e3aaec3e4849c9fec993efd2c6be0ae169
Maulana Arya Alambana <67086869+MaulanaArya30@users.noreply.github.com>
2022-10-31 16:42:08 +0700

    Add files via upload

---
+++ Web App/website/auth.py
@@ -0,0 +1,67 @@
+#import email
+#from unicodedata import category
+#from click import password_option
+from contextlib import redirect_stderr
+from flask import Blueprint, render_template, request, flash, redirect, url_for
+#session
+from .models import User
+from werkzeug.security import generate_password_hash, check_password_hash
+from . import db
+from flask_login import login_user, login_required, logout_user, current_user
+
+auth = Blueprint('auth', __name__)
+
+@auth.route('/login', methods=['GET', 'POST'])
+def login():
+	if request.method == 'POST':
+		email = request.form.get('email')
+		password = request.form.get('password')
+
+		user = User.query.filter_by(email=email).first()
+		if user:
+			if check_password_hash(user.password, password):
+				flash('Login successful', category='success')
+				login_user(user, remember=True)
+				return redirect(url_for('views.home'))
+			else:
+				flash('Login failed', category='error')
+		else:
+			flash('Email not found', category='error')
+
+	return render_template('login.html', user=current_user)
+
+@auth.route('/logout')
+@login_required
+def logout():
+	logout_user()
+	return redirect(url_for('auth.login'))
+
+@auth.route('/sign-up', methods=['GET', 'POST'])
+def sign_up():
+	if request.method == 'POST':
+		email = request.form.get('email')
+		first_name = request.form.get('firstname')
+		password1 = request.form.get('password1')
+		password2 = request.form.get('password2')
+
+		user = User.query.filter_by(email=email).first()
+		if user:
+			flash('Email already exists', category='error')
+		elif len(email) <5:
+			flash('Email is too short', category='error')
+		elif len(first_name) < 2:
+			flash('First name is too short', category='error')
+		elif password1 != password2:
+			flash('Password does not match', category='error')
+		elif len(password1) < 7:
+			flash('Password is too short', category='error')
+		else:
+			new_user = User(email=email, first_name=first_name, password=generate_password_hash(password1, method='sha256'))
+			db.session.add(new_user)
+			db.session.commit()
+			flash('Account is created', category='success')
+			login_user(user, remember=True)
+			return redirect(url_for('views.home'))
+
+
+	return render_template('sign_up.html', user=current_user)
